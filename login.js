1f44842273392cf1d2273b241099fc598743604e
dimantha2004 <Dimanthatheekshana12345@gmail.com>
2025-03-04 00:26:09 +0530

    final commit

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-// Toggle between Login and Signup forms
 document.getElementById('show-signup').addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector('.login-container').style.display = 'none';
@@ -11,7 +10,6 @@ document.getElementById('show-login').addEventListener('click', function (e) {
     document.querySelector('.login-container').style.display = 'block';
 });
 
-// Login Form Submission
 document.getElementById('login-form').addEventListener('submit', function (e) {
     e.preventDefault();
     const username = document.getElementById('USERNAME').value;
@@ -41,34 +39,3 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
         console.error(error);
     });
 });
-
-// document.getElementById('login-form').addEventListener('submit', function (event) {
-//     event.preventDefault();
-//     const username = document.getElementById('USERNAME').value;
-//     const password = document.getElementById('PASSWORD').value;
-
-//     fetch('http://localhost:8080/api/users/login', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//         },
-//         body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
-//     })
-//     .then(response => {
-//         if (response.ok) {
-//             return response.text(); 
-//         } else {
-//             throw new Error('Login failed');
-//         }
-//     })
-//     .then(message => {
-//         alert(message); 
-        
-//         document.querySelector('.login-container').style.display = 'none';
-//         document.getElementById('app-content').style.display = 'block';
-//     })
-//     .catch(error => {
-//         alert('Incorrect username or password. Please try again.');
-//         console.error(error);
-//     });
-// });
